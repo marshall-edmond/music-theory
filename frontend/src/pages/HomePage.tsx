3087e9f169957fc40e696968f62dd97266630934
marshall-edmond <marshalledmond05@gmail.com>
2025-11-08 12:43:32 -0500

    using .module.css to import styles

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,9 +1,9 @@
-import '/styles/HomePage.css'
+import styles from '../styles/HomePage.module.css'
 
 function Home(){
     return (
-        <div className="title">
-            <title>Music Through Artistry</title>
+        <div className={styles.h1}>
+            <h1>Music Through Artistry</h1>
         </div>
     )
 }
