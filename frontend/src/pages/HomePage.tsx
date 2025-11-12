395be5c31607264ba8a0f6662bf8e5580d7fa350
marshall-edmond <marshalledmond05@gmail.com>
2025-11-11 22:34:30 -0500

    Adding Mission section to frontend

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -2,10 +2,18 @@ import styles from '../styles/HomePage.module.css'
 
 function Home(){
     return (
+        <div>
         <div className={styles.h1}>
-            <h1>Music Through Artistry</h1>
+            <h1>Music Through <span className={styles.highlight}></span>Artistry</h1>
+        </div>
+        <div className={styles.h2}>
+            <h2>Mission</h2>
+        </div>
+        <div className={styles.p}>
+            <p>musicmap is an application designed for beginner musicians. We specialize on teaching music theory through artistry. Ready to get started on your theory journey?</p>
+        </div>
         </div>
     )
-}
+}   
 
 export default Home;
