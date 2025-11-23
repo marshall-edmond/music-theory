b4d1fbe3c2ea4f469fbde77f03b5363e6f2cbcb5
marshall-edmond <marshalledmond05@gmail.com>
2025-11-23 00:03:41 -0500

    Naming conventions + restructuring

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,6 +9,7 @@ import './App.css'
 function App (){ 
   return (
     <>
+
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/theory" element={<Theory />} /> 
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,13 +1,36 @@
 
 import Header from '../components/Header';
-import Hero from '../components/Hero';
+import styles from '../styles/HomePage.module.css';
+import { Link } from 'react-router-dom';
 
 
 function HomePage(){
     return (
         <div>
             <Header />
-            <Hero />
+            <div className={styles.mainContainer}>
+                <div className={styles.hero}>
+                    <div className={styles.h1}>
+                        <h1>Music Through Artistry</h1>
+                    </div>
+                    <div className={styles.details}>
+                        <div className={styles.p}>
+                            <p>Learning music theory doesn't have to feel like memorizing rules</p>
+                        </div>
+                        <div className={styles.p}>
+                            <p>Try out our Theory Roadmap which will teach you musical concepts through some of your favorite songs</p>
+                        </div>
+                    </div>
+                    <div className={styles.btnContainter}>
+                        <div className={styles.btn}>
+                            <Link className={styles.btn} to="/theory">Try Now</Link>
+                        </div>
+                        <div className={styles.btn2}>
+                            <Link className={styles.btn2} to="/about">About</Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }   
