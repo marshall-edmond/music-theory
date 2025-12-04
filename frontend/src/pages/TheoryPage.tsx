c2ddf9a3fba6bd3af719fd35ae9c308ac18c96ba
marshall-edmond <marshalledmond05@gmail.com>
2025-12-04 01:06:38 -0500

    Updated adding theory section

--- frontend/src/pages/TheoryPage.tsx
+++ frontend/src/pages/TheoryPage.tsx
@@ -1,11 +1,17 @@
-import FakeHeader from '../components/fakeheader.tsx';
+import Header from '../components/Header.tsx';
+import styles from '../styles/Theory.module.css';
 
 
 
 function Theory(){
     return (
         <div>
-            <FakeHeader />
+            <Header/>
+            <div className={styles.mainContainer}>
+                <div className={styles.sidebar}>
+                </div>
+
+            </div>
         </div>
     )
 }
