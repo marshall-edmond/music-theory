55efe6c4372d5cd682b2e32ee0d2cf3bd4190f2b
marshall-edmond <marshalledmond05@gmail.com>
2026-01-01 02:39:15 -0500

    Second lessong page is complete, moving to the third page

--- frontend/src/components/lessons/Notes.tsx
+++ frontend/src/components/lessons/Notes.tsx
@@ -14,9 +14,9 @@ export default function Notes () {
                     <GiGrandPiano size={32}/>
                 </div>
             <p className={styles.p}>Music is the universal language of the world. Meaning we can all speak it regardless of factors like age, culture, gender & so on. Like any other language, music has a foundation as well as rules which generally shouldn't be broken. The entire western world of music rests on the shoulders of the first 7 letters of our alphabet.</p>
-            <p className={styles.p}>A, B, C, D, E, F, G</p>
+            <p className={styles.p} style={{fontWeight:'bold'}}> A, B, C, D, E, F, G</p>
             <p className={styles.p}>It's hard to fathom that every single song is simply made from these letters (although its true). Within music these letters are referred to as notes. Every note consists of a letter which maps the sound as well as a number to represent the frequency of the sound.</p>
-            <p className={styles.p}>ex: C4</p>
+            <p className={styles.p}style= {{fontWeight:'bold'}}>ex: C4</p>
             <p className={styles.p}>Notes are the building block to all forms of music. The 7 previously mentioned letters are called Natural notes because they have no accidentals. An accidental takes the pitch of a note and slightly alters it. The types of accidentals are flats(♭), sharps(#), double flats(♭♭), and double sharps(♯♯).</p>
         </>
     )
--- frontend/src/components/lessons/Notes1.tsx
+++ frontend/src/components/lessons/Notes1.tsx
@@ -11,11 +11,20 @@ export default function Notes1() {
             <h1 className={styles.h1}>Notes</h1>
             <section className={styles.pianoIcon}>
                 <GiGrandPiano size={32}/>
+                <p className={styles.pianoIcon}>3 min. reading</p>
             </section>
             <p className={styles.p}>
-                Alright, now that we know what notes are lets see how they interact on a piano. The white keys on the piano hold the natural notes (A,B,C,D,E,F,G) and the black keys hold the accidentals(♭, #).
+                Alright, now that we know what notes are let's see how they interact on a piano. The white keys on the piano hold the natural notes (A,B,C,D,E,F,G) and the black keys hold the accidentals(♭, #). The C4 note sits in the middle of a standard 88 key keyboard, and it is recognized as being the white key before the two black keys.
             </p>
-
+            <p className={styles.p}>
+                <b>Sharp(#):</b> The note immediately to the right is given a sharp (higher in pitch).
+            </p>
+            <p className={styles.p}>
+                <b>Flat(♭):</b> The note immediately to the left is given a flat (lower in pitch).
+            </p>
+            <p className={styles.p}>
+                These previously discussed 12 notes, black and white, repeat around the entirety of the piano. The black notes have two different names but they are named relative to the notes next to them. They are given either a flat(♭), or a sharp(#) which indicates whether it's before or after the note. For example, the black note directly to the right of the C is called a C#, but it can also be referred to as a D♭. This is called <b>Enharmonic Equivalent</b>,  we'll go more in depth about the rules of this later, but for now it's important to practice naming the black notes.
+            </p>
             
         </>
 
