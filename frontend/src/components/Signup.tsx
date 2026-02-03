d1b2cb6514a88a4e033b2073b83338afcf734a51
marshall-edmond <marshalledmond05@gmail.com>
2026-02-03 12:08:39 -0500

    Refactored signup page

--- frontend/src/components/Signup.tsx
+++ frontend/src/components/Signup.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import type { FormEvent } from 'react';
 import { useNavigate } from 'react-router';
 import styles from '../styles/Signup.module.css';
 import { FaLock } from 'react-icons/fa';
@@ -16,59 +17,74 @@ function Signup(){
     //navigation function
     const nav = useNavigate()
     
-    async function HandleSubmit(){
+    async function HandleSubmit(e: FormEvent<HTMLFormElement>) {
+        e.preventDefault()
+
+        if (password !== confirm) {
+                setError('Passwords do not match.')
+                return
+            }
+
+
         try {
             // fetch signup endpoint
             setLoading(true);
-            const response = await fetch(`${import.meta.env.VITE_API_URL}/signup`, {method: "POST", body: JSON.stringify({username, password, email})});
+
+            const response = await fetch(`${import.meta.env.VITE_API_URL}/signup`, {method: "POST", headers: {'Content-type': "application/json"} , body: JSON.stringify({username, password, email})});
             const result = await response.json();
 
             // if user signup is successful, navigate to the home screeen
             if (response.ok){
                 nav('/');
-
             // if user sign up is unsuccessful, set and display error message
                 } else {
                     setLoading(false); 
-                    setError(result);
-
-                    return (
-                        <div className={styles.error}>{error}</div>
-                    )
+                    setError(result.message);
                 }
-            }
-
-        catch (err) {
-            return (
-                <div className={styles.error}>{error}</div>
-            )
-
-        }
-
-
-
+            } catch (err) {
+                setError('Network Error')
+        } finally {
+            setLoading(false)
+    
     }
+}
 
     return (
         
-        <div className={styles.signup}>
+        <form className={styles.signup} onSubmit={HandleSubmit}>
             <h2> Username </h2>
-            <input placeholder="John Doe" value={username} onChange={e => setUsername(e.target.value)}></input>
+            <input placeholder="John Doe"
+            value={username}
+            onChange={e => setUsername(e.target.value)}
+            />
+
 
             <h2 >Email Address </h2>
-            <input type="email" placeholder="you@example.com" value={email} onChange={e => setEmail(e.target.value)}></input>
+            <input type="email"
+            placeholder="you@example.com"
+            value={email}
+             onChange={e => setEmail(e.target.value)}/>
 
             <h2>Password</h2>
-            <input type="password" value={password} onChange={e => setPassword(e.target.value)}></input>
+            <input type="password"
+            value={password}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}/>
 
             <h2> Confirm Password </h2>
-            <input type="password" value={confirm} onChange={(e => setConfirm(e.target.value))}><FaLock /></input>
+            <div className={styles.passwordRow}>
+                <input type="password"
+                value={confirm} 
+                onChange={(e => setConfirm(e.target.value))}/>
+                <FaLock/>
+            </div>
 
-            <button className={styles.button} type="button" onSubmit={() => HandleSubmit()}> Create Account </button>
-        </div>
+            <button className={styles.button} type="submit" disabled={loading}>
+                {loading ? 'Creating...' : 'Create Account'}
+            </button>
 
 
-
+            {error && <div className={styles.error}>{error}</div>}
+        </form>
     )}
 
 export default Signup;
