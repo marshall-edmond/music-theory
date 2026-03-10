67e58496d36ab54243d6260d321cd86ab823eea0
marshall-edmond <marshalledmond05@gmail.com>
2026-03-10 15:12:46 -0400

    Question 1 added

--- frontend/src/components/Quizzes/NotesQuiz.tsx
+++ frontend/src/components/Quizzes/NotesQuiz.tsx
@@ -1,12 +1,21 @@
-import styles from '../../styles/Quiz.module.css'
+import styles from '../../styles/Quiz.module.css';
+import Piano from '../Piano.tsx';
+import { randomNotes } from '../../music/Note.tsx';
 
 
 export default function NotesQuiz(){
+
+    const note = randomNotes();
     
     return (
         <div className={styles.mainContainer}>
+            <div className={styles.intro}>Notes Quiz</div>
+            <div className={styles.questionBox}>
+                <div className={styles.question1}>1. Identify the Note </div>
+                <div className={styles.Note}> { note } </div>
+                <Piano />
+
+            </div>
         </div>
     )
-
-
 }
---
+++ frontend/src/music/Note.tsx
@@ -0,0 +1,7 @@
+//list of notes including all accidentals
+export const ListNotes = ['C', 'C#', 'D♭', "D", "D#", 'E♭', 'E', 'F', 'F#', 'G♭', 'G', 'G#', 'A♭', 'A', 'A#', 'B♭', 'B']
+
+export const randomNotes = () =>{
+    return ListNotes[Math.floor(Math.random() * ListNotes.length)];
+    
+} 
