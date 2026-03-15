a30b0a39b593c988cf2d8255fa276b1fa5e52674
marshall-edmond <marshalledmond05@gmail.com>
2026-03-15 14:13:07 -0400

    Fix - State variables added

--- frontend/src/components/Quizzes/NotesQuiz.tsx
+++ frontend/src/components/Quizzes/NotesQuiz.tsx
@@ -4,15 +4,16 @@ import { randomNotes } from '../../music/Note.tsx';
 import { useState } from 'react';
 
 
-
-
-
 export default function NotesQuiz(){
     //state variable for activeNote
     const [activeNotes, setActiveNote] = useState('');
     //correct or incorrect
-    const [response, setResponse] = useState(false)
+    const [response, setResponse] = useState(false);
     const [ note ] = useState(randomNotes);
+
+    //question2
+    const [response2, setResponse2] = useState('');
+    const [display1, setDisplay1] = useState(false);
 
 
     //check if selected piano note matches the generated note
@@ -21,6 +22,10 @@ export default function NotesQuiz(){
         if (note == activeNotes){
             setResponse(true);
         }
+    }
+    //check answer
+    const Question2 = () =>  {
+        setDisplay1(true);
     }
     
     return (
@@ -49,6 +54,27 @@ export default function NotesQuiz(){
             </div>
 
             <div className={styles.questionBox}>
+                <div className={styles.question1}>2. How many Notes are in an Octave? </div>
+                <div className={styles.btnContainer}>
+                    <div className={styles.btn2}>12</div>
+                    <div className={styles.btn2}>10</div>
+                    <div className={styles.btn2}>6</div>
+                    <div className={styles.btn2}>8</div>
+                </div>
+
+                <div className={styles.btn}
+                onClick={() => Question2()}
+                >
+                Check Answer
+                </div>
+
+
+             
+            </div>
+
+            <div className={styles.questionBox}>
+                <div className={styles.question1}>How many differnet notes are on a piano (including accidentals)? </div>
+                <div className={styles.btnContainer}></div>
             </div>
         </div>
     )
