d4454c90920c3f0aba054d759b9efaeb8749ea48
marshall-edmond <marshalledmond05@gmail.com>
2026-04-07 15:50:56 -0400

    Added - Scale Rules

--- frontend/src/components/lessons/keysandscales/ScaleRules.tsx
+++ frontend/src/components/lessons/keysandscales/ScaleRules.tsx
@@ -11,8 +11,8 @@ export default function ScaleRules(){
                     <GiGrandPiano size={32}/>
                 </section>
                 <p className={styles.p}>The first rule is that every major and minor scale must contain one and only one of each letter. Meaning if we're building a major or minor scale starting from A the scale must also contain a variation of B, C, D, E, F, and G.</p>
-                <p className={styles.p}>Remember enharmonic equivalents? They come into play here as every scale (not just major and minor) can only have one type of accidental, meaning it can either can have sharps or flats, but not both.</p>
-                <p className={styles.p}>Scales are only played in ascending or decending order.</p>
+                <p className={styles.p}>Remember enharmonic equivalents? They come into play here as every scale (not just major and minor) can only have one type of accidental, meaning it can either can have sharps or flats, but not both. For example if we've already used a F in a scale and the next note is an F#, we would default to the enharmonic equivalent (Gb) to include every letter once.</p>
+                <p className={styles.p}>Scales are only played in ascending or decending order. Every major and minor scale contains 7 notes before reaching the octave of the root.</p>
             </>
         )
 
