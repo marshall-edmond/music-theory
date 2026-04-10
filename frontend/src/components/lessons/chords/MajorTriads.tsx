5a91a0efda27ce88cbba28142c43d76039d85391
marshall-edmond <marshalledmond05@gmail.com>
2026-04-10 00:17:25 -0400

    Added - Major Tried generator

--- frontend/src/components/lessons/chords/MajorTriads.tsx
+++ frontend/src/components/lessons/chords/MajorTriads.tsx
@@ -3,15 +3,21 @@ import Piano from '../../Piano';
 import { GiGrandPiano } from 'react-icons/gi';
 import styles from '../../../styles/Theory.module.css';
 import { useState } from 'react';
-import { majorTriads } from '../../../music/MajorTr';
+import { MajorTr } from '../../../music/MajorTr.tsx';
 
-const [triad, setTriad] =  useState<string[]>([]);
 
-//set state of triad by generating major triad for the note.
-const getTriad = (root: string) => {
-    setTriad(majorTriads(root))
-}
 export default function MajorTriads(){
+
+    //chord to pass to onNoteSelect
+    const [triad, setTriad] =  useState<string[]>([]);
+
+    //set state of triad by generating major triad for the note.
+    const getTriad = (root: string) => {
+        const scale = MajorTr(root)
+        console.log(scale)
+        setTriad(scale)
+
+}
     const leftContent = (
         <>  
             <h1 className={styles.h1}>Major Triads</h1>
---
+++ frontend/src/music/MajorTr.tsx
@@ -0,0 +1,11 @@
+import { majorScale } from './MajorFormula';
+
+export const MajorTr = (root:string) => {
+
+    //get the major scale of the root and slice to return triad
+    const scale = majorScale(root)
+    const triad = [scale[0], scale[2], scale[4]]
+
+    return triad;
+    
+}
