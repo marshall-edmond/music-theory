03027cb107e32e709a026df018ca2a233ce39811
marshall-edmond <marshalledmond05@gmail.com>
2026-04-13 10:00:02 -0400

    Added - Finished Minor Triads

--- frontend/src/components/lessons/chords/MinorTriads.tsx
+++ frontend/src/components/lessons/chords/MinorTriads.tsx
@@ -22,6 +22,9 @@ export default function MinorTriads(){
                 <GiGrandPiano size={32}/>
                 <p className={styles.piano}>2 min. reading</p>
             </section>
+            <p className={styles.p}>The minor chord/triad is built on the minor scale. Just like the major triad, the minor triad is built on the 1st, 3rd and 5th degrees of the minor scale.</p>
+            <p className={styles.p}>Also worth noting, a minor chord is the equivalent of taking the major chord of the root and lowering the 3rd by a semitone.</p>
+            <p className={styles.p}>Select any note on the piano to generate the minor chord/triad of the root.</p>
         </>
     )
     const rightContent = (
